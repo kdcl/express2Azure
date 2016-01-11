c7b78272611ca82e52fa84847ad9814f5376b154
kdcl <kdcl03@gmail.com>
2016-01-11 23:57:55 +0800

    parse bus data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var request = require('request');
+
+var zlib = require('zlib');
 var router = express.Router();
+
+var json;""
+var options = {
+  url: 'http://data.taipei/bus/CarUnusual',
+  headers: {
+    'X-some-headers'  : 'Some headers',
+    'Accept-Encoding' : 'gzip, deflate',
+  },
+  encoding: null
+};
+
+
+request.get(options, function (error, response, body) {
+ 
+  if (!error && response.statusCode == 200) {
+    // If response is gzip, unzip first
+    var encoding = response.headers['content-encoding']
+    if (encoding && encoding.indexOf('gzip') >= 0) {
+      zlib.gunzip(body, function(err, dezipped) {
+        var json_string = dezipped.toString('utf-8');
+        json = JSON.parse(json_string);
+        console.log(json.BusInfo[0].BusID);
+        // Process the json..
+      });
+    } else {
+      // Response is not gzipped
+    }
+  }
+ 
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Windows Azure' });
+  res.render('index', { title: json.BusInfo[0].BusID });
 });
 
 module.exports = router;
